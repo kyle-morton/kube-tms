a578bf4860cc416035271aeb77d18ae1a5bd134c
Kyle Morton <kylemortondevelopment@gmail.com>
2021-12-13 21:12:19 -0600

    working on tms -> carrier microservice

--- KubeTMS/KubeTMS.Core/Services/CarrierService.cs
+++ KubeTMS/KubeTMS.Core/Services/CarrierService.cs
@@ -1,6 +1,8 @@
 ﻿using KubeTMS.Core.Data;
 using KubeTMS.Shared.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 
 namespace KubeTMS.Core.Services
 {
@@ -11,13 +13,39 @@ namespace KubeTMS.Core.Services
 
     public class CarrierService : ServiceBase, ICarrierService
     {
-        public CarrierService(KubeTMSDbContext context) : base(context)
+        private readonly IConfiguration _config;
+
+        public CarrierService(KubeTMSDbContext context, IConfiguration config) : base(context)
         {
+            _config = config;
         }
 
         public async Task<List<Carrier>> GetAsync()
         {
-            return await _context.Carriers.ToListAsync();
+            var url = _config["ApiUrls:Carriers"] + "carriers";
+            Console.WriteLine("--> Getting Carriers: " + url);
+
+            var carriers = new List<Carrier>();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response: " + responseBody);
+                    carriers = JsonSerializer.Deserialize<List<Carrier>>(responseBody);
+                }
+            }
+
+            return carriers;
         }
+
+        //public async Task<List<Carrier>> GetAsync()
+        //{
+        //    return await _context.Carriers.ToListAsync();
+        //}
+
+
     }
 }
---
+++ KubeTMS/KubeTMS.Server/Controllers/CarriersController.cs
@@ -0,0 +1,27 @@
+﻿using KubeTMS.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KubeTMS.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarriersController : ControllerBase
+    {
+
+        private readonly ILogger<CarriersController> _logger;
+        private readonly ICarrierService _service;
+
+        public CarriersController(ICarrierService service, ILogger<CarriersController> logger)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var carriers = await _service.GetAsync();
+            return Ok(carriers);
+        }
+    }
+}
